28ef979581e2fafb8cd381598947b6780b2e87c5
Bozhidar Ivanov <bozhidarivanov20a@gmail.com>
2025-03-29 01:07:20 +0200

    second commit

--- CatManager3000/backend/firebase.js
+++ CatManager3000/backend/firebase.js
@@ -1,10 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json'); // Името на вашия файл
 
+// Safely load from environment variables
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://catmanager300.firebaseio.com" // Заменете с вашия project_id
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
-const db = admin.firestore();
-module.exports = db;
+module.exports = admin.firestore();
